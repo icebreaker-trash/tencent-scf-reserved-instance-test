1f889d9e954af55b1fe3db6ad8bce11a051383dd
SonOfMagic <qq1324318532@gmail.com>
2021-05-16 21:32:06 +0800

    feat: add more test case

---
+++ benchmark/index.js
@@ -0,0 +1,6 @@
+const address = {
+  0: 'http://service-j7h4dzet-1257725330.sh.apigw.tencentcs.com:80',
+  10: 'http://service-e2znk8sp-1257725330.sh.apigw.tencentcs.com:80',
+  20: 'http://service-kaglrinf-1257725330.sh.apigw.tencentcs.com:80',
+  'warmup-5': ''
+}
